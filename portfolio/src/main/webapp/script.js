b07d75298ec84181baa10c1e350da43e0452039f
Ricardo Paz <paz.ricardo34@gmail.com>
2020-03-09 15:50:22 -0400

    Map is now visible

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -36,5 +36,5 @@ function getRandomQuoteUsingArrowFunctions() {
 function createMap() {
   const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 8});
+      {center: {lat: 37.422, lng: -122.084}, zoom: 10});
 }
